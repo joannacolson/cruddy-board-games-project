1539143564b33272af901da30ecab1171689ed99
Joanna Colson <joannacolson@gmail.com>
2017-04-20 17:29:32 -0700

    completed work

--- app.js
+++ app.js
@@ -19,9 +19,101 @@ app.set('view engine', 'ejs');
 
 // your routes here
 
-// ...
+// redirect get of the root URL to the /games route
+app.get('/', function(req, res) {
+    res.redirect('/games');
+});
+
+// display a list of all games, action: index
+app.get('/games', function(req, res) {
+    // get all the games
+    var games = getGames();
+    // console.log(games[0]);
+    res.render('games-index', { games: games });
+});
+
+// return an HTML form for creating a new game
+app.get('/games/new', function(req, res) {
+    res.render('games-new');
+
+});
+// create a new game (using form data from /games/new)
+// does not need its own page, only adds game to array
+app.post('/games', function(req, res) {
+    console.log(req.body);
+    var newGame = req.body;
+
+    var games = getGames();
+    games.push(newGame);
+    saveGames(games);
+
+    res.redirect('/games');
+});
+
+// show or display a specific game
+app.get('/games/:name', function(req, res) {
+    // set variable = the name parameter
+    var nameOfTheGame = req.params.name;
+    // console.log(nameOfTheGame);
+    var games = getGames();
+    var game = getGame(games, nameOfTheGame);
+
+    res.render('games-show', game);
+});
+
+// return an HTML form for editing a game
+app.get('/games/:name/edit', function(req, res) {
+    var nameOfTheGame = req.params.name;
+    var games = getGames();
+    var game = getGame(games, nameOfTheGame);
+
+    res.render('games-edit', game);
+});
+
+// update a specific game (using form data from /games/:name/edit)
+app.put('/games/:name', function(req, res) {
+    var theNewGameData = req.body;
+
+    var nameOfTheGame = req.params.name;
+    var games = getGames();
+    var game = getGame(games, nameOfTheGame);
+
+    game.name = theNewGameData.name;
+    game.description = theNewGameData.description;
+
+    saveGames(games);
+
+    res.send(req.body);
+});
+
+// deletes a specific game
+app.delete('/games/:name', function(req, res) {
+    var nameOfTheGame = req.params.name;
+    var games = getGames();
+    var game = getGame(games, nameOfTheGame);
+    var indexOfGameToDelete = games.indexOf(game);
+
+    games.splice(indexOfGameToDelete, 1);
+
+    saveGames(games);
+
+    res.send(game);
+});
 
 // helper functions
+
+// This is my function: get a specific game
+function getGame(games, nameOfTheGame) {
+    var game = null;
+
+    for (var i = 0; i < games.length; i++) {
+        if (games[i].name.toLowerCase() == nameOfTheGame.toLowerCase()) {
+            game = games[i];
+            break;
+        }
+    }
+    return game;
+}
 
 // Read list of games from file.
 function getGames() {
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,47 @@
-console.log("JS good to go, sir!");
+$(function() {
+    console.log("JS good to go, sir!");
 
-// listen for a "submit" event for the edit page. the edit form will live
-// at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
-// appropriate URL
+    // listen for a "submit" event for the edit page. the edit form will live
+    // at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
+    // appropriate URL
 
-// code here ...
+    $('#edit-game').on('submit', function(event) {
+        event.preventDefault();
+        var element = $(this);
+        var url = element.attr('action');
+        var method = element.attr('method');
+        var formData = element.serialize();
 
-// listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
-// to the appropriate URL
+        console.log(formData);
+        console.log(method);
 
-// code here ...
+        $.ajax({
+            method: method,
+            url: url,
+            data: formData
+        }).done(function(data) {
+            // get data returned from the PUT route
+            console.log(data);
+
+            // refresh the page we're on using GET to display the item details.
+            window.location = '/';
+        });
+    });
+
+
+    // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
+    // to the appropriate URL
+
+    $('.delete-link').on('click', function(event) {
+        event.preventDefault();
+        var url = $(this).attr('href');
+
+        $.ajax({
+            url: url,
+            method: 'DELETE'
+        }).done(function() {
+            window.location = '/';
+        });
+    });
+
+});
